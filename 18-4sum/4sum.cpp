1118d06f4b41dee8c7040d665b64a06c22ad72d5
Pulkit Aluna <pulkitaluna187@gmail.com>
2026-08-01 15:04:11 +0530

    Time: 23 ms (53.97%) | Memory: 17.3 MB (99.19%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -5,34 +5,33 @@ public:
         vector<vector<int>> ans;
         sort(nums.begin(),nums.end());
         for(int i=0;i<n;i++){
-            if(i>0 && nums[i-1]==nums[i]) continue;
+            if(i>0 && nums[i]==nums[i-1]) continue;
             for(int j=i+1;j<n;){
                 int k=j+1;
                 int l=n-1;
                 while(k<l){
-                    long long sum= (long long)nums[i] + (long long)nums[j] + 
-                    (long long)nums[k] + (long long)nums[l];
-
-                if(sum==target){
-                    ans.push_back({nums[i],nums[j],nums[k],nums[l]});
-                    k++;
-                    l--;
-                    while(k<l && nums[k]==nums[k-1]){
+                    long long sum = (long long)nums[i]+(long long)nums[j]+(long long)nums[k]+(long long)nums[l];
+                    if(sum==target){
+                        ans.push_back({nums[i],nums[j],nums[k],nums[l]});
+                        k++;
+                        l--;
+                        while(k<l && nums[k]==nums[k-1]){
+                            k++;
+                        }
+                    }
+                    else if(sum<target){
                         k++;
                     }
+                    else{
+                        l--;
+                    }
                 }
-                else if(sum<target){
-                    k++;
+                 j++;
+                while(j<n && nums[j]==nums[j-1]){
+                         j++;
                 }
-                else{
-                    l--;
-                }
-            }
-            j++;
-            while(j<n && nums[j]==nums[j-1]) j++;
             }
         }
         return ans;
-        
     }
 };
