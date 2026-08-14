d44380866769208c7a337f230cacda01c0500cb9
Pulkit Aluna <pulkitaluna187@gmail.com>
2026-08-14 10:49:01 +0530

    Time: 0 ms (100.00%) | Memory: 65.2 MB (41.85%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -3,17 +3,16 @@ public:
     int findDuplicate(vector<int>& nums) {
         int slow=nums[0];
         int fast=nums[0];
+
         do{
             slow=nums[slow];//+1
-            fast=nums[nums[fast]];//+2
-        } while(slow!=fast);
-
+            fast=nums[nums[fast]]; //+2
+        } while(slow !=fast);
         slow=nums[0];
         while(slow!=fast){
             slow=nums[slow];
             fast=nums[fast];
         }
-        return slow;
-        
+            return slow;
     }
 };
