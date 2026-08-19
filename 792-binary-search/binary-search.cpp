1cd4266cffe8880ed75f022d2854334d99145cd8
Pulkit Aluna <pulkitaluna187@gmail.com>
2026-08-19 09:56:59 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (46.53%) - LeetSync

---
+++ 792-binary-search/binary-search.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int st=0;
+        int end=n-1;
+        while(st<=end){
+            int mid=st+end;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                end=mid-1;
+            }
+            else{
+                st=mid+1;
+            }
+        }
+        return -1;
+    }
+};
