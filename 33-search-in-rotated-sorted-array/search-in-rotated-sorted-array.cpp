fbd4e6dd56dc18b8e706115c1d8b1f0c3aedd9db
Pulkit Aluna <pulkitaluna187@gmail.com>
2026-08-20 23:32:14 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (69.01%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,41 +1,31 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        return binarySearch(nums, 0, nums.size() - 1, target);
-    }
-
-    int binarySearch(vector<int>& arr, int si, int ei, int target) {
-
-        if (si > ei) {
-            return -1;
-        }
-
-        int mid = si + (ei - si) / 2;
-
-        if (arr[mid] == target) {
-            return mid;
-        }
-
-        
-        if (arr[si] <= arr[mid]) {
-
-            if (arr[si] <= target && target < arr[mid]) {
-                return binarySearch(arr, si, mid - 1, target);
+        int n=nums.size();
+        int st=0;
+        int end=n-1;
+        while(st<=end){
+            int mid=st + (end-st)/2;
+            if(nums[mid]==target){
+                return mid;
             }
-            else {
-                return binarySearch(arr, mid + 1, ei, target);
+            if(nums[st]<=nums[mid]){
+                if(nums[st]<=target && target < nums[mid]){
+                    end=mid-1;
+                }
+                else{
+                    st=mid+1;
+                }
             }
-        }
-
-        
-        else {
-
-            if (arr[mid] < target && target <= arr[ei]) {
-                return binarySearch(arr, mid + 1, ei, target);
-            }
-            else {
-                return binarySearch(arr, si, mid - 1, target);
+            else{
+                if(nums[mid]<target && target <= nums[end]){
+                    st=mid+1;
+                }
+                else{
+                    end=mid-1;
+                }
             }
         }
+        return -1;
     }
 };
